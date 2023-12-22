c2817e3048df6c4fdccc2a8dab4d12de91bc692b
Themis <themistoklisarv2001@gmail.com>
2023-12-22 12:06:21 +0200

    all buttons are save on before

--- cypress/e2e/sample_test.js
+++ cypress/e2e/sample_test.js
@@ -2,4 +2,5 @@ describe('First test', () => {
     it('Visit Kartgreen', () => {
         cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
     })
+    it
 })
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,14 +1,10 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
-
-    // cy.get('.product:nth-child(2) .product-action button').click();
-
-    cy.get('.product:first .product-action button').as('firstAddToCartButton');
-
-    cy.get('@firstAddToCartButton').click();
-
-
+  beforeEach(() => {
+    // Cypress starts out with a blank slate for each test
+    // so we must tell it to visit our website with the `cy.visit()` command.
+    // Since we want to visit the same URL at the start of all our tests,
+    // we include it in our beforeEach function so that it runs before each test
+    cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/cart')
     for (let i = 1; i <= 29; i++) {
       const aliasName = `addToCartButton${i}`; //this is for the add to cart button
       const numInput = `quantityInput${i}`; //this is for quantity
@@ -20,16 +16,37 @@ describe('template spec', () => {
       cy.get(`.product:nth-child(${i}) .stepper-input a.decrement`).as(decrement);
       cy.get(`.product:nth-child(${i}) .stepper-input a.increment`).as(increment);
     }
+  })
+  it('passes', () => {
+    cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
+
+    // cy.get('.product:nth-child(2) .product-action button').click();
+
+    cy.get('.product:first .product-action button').as('firstAddToCartButton');
+
+    cy.get('@firstAddToCartButton').click();
+
+
+
 
     cy.get('@addToCartButton3').click();
     cy.get('@quantityInput20').clear().type('3'); //testing to see if that's right
     cy.get('@decrementLink20').click();
     cy.get('@incrementLink21').click();
     
+  })
 
 
 
+  it("US-2", () =>{
 
+    cy.get('@addToCartButton3').click();
+    cy.get('@quantityInput20').clear().type('3'); //testing to see if that's right
+    cy.get('@decrementLink20').click();
+    cy.get('@incrementLink21').click();
+    cy.get(".cart-icon").click()
+    // cy.get("PROCCEED TO CHECKOUT").click()
+    // cy.get("productCartTables").children().length()
   })
 })
 
